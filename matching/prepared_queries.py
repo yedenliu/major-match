aabaeed1eb9b488e3a552898ada9cc2149851866
Eden Liu <edenliu100@gmail.com>
2022-04-11 22:14:45 -0400

    front-end of match page first pass

--- matching/prepared_queries.py
+++ matching/prepared_queries.py
@@ -2,10 +2,44 @@ from tkinter import ttk
 import cs304dbi as dbi
 
 '''
-Accept the dept abbr and course number from form data
-Match course to database courses 
+1. Accept the dept abbr and course number from form data --> put into DB 
+2. Match course to database courses 
 Get majors on matched courses 
 Get count of each major that is listed the most 
 '''
 
+def find_cid(conn, dept, cnum):
+    curs = dbi.cursor(conn)
+    sql = '''   select cid from courses
+                where dept = %s and cnum = %s
+            '''
+    curs.execute(sql, [dept, cnum])
+    row = curs.fetchone()
+    return row
 
+def insert_data(conn, dept, cnum):
+    curs = dbi.cursor(conn)
+    if dept != None and cnum != None:
+        cid = find_cid(conn, dept, cnum)
+        sql = '''   insert into form_data(dept, cnum, cid)
+                    values (%s, %s, %s)
+                ''' 
+        curs.execute(sql, [dept, cnum, cid])
+        conn.commit()
+
+def major_match(conn):
+    curs = dbi.cursor(conn)
+    sql = '''   select programs.name, count(major_pairs.dept_id) from programs
+                inner join major_pairs using(dept_id)
+                inner join form_data using(cid)
+                group by major_pairs.dept_id
+                limit 5
+            ''' 
+    curs.execute(sql)
+    return curs.fetchall()
+
+def delete_form_data(conn):
+    curs = dbi.cursor(conn)
+    sql = 'delete from form_data'
+    curs.execute(sql)
+    conn.commit()
