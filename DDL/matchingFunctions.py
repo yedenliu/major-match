04257bd120a51e04b976a7e80c96dbbc953143ce
Kat Swint <kswint@wellesley.edu>
2022-04-20 13:25:38 -0400

    draft version of CS major matching complete

--- DDL/matchingFunctions.py
+++ DDL/matchingFunctions.py
@@ -4,11 +4,88 @@ import csv
 majorReqs = '/students/kswint/major-match/DDL/majorReqsDF.tsv'
 coursesToMajors = '/students/kswint/major-match/DDL/coursesToMajors.tsv'
 
-def cs(userInput, majorRow):
-    introductorySequence = ['CS 111','CS 230']
-    math = ['MATH 225']
-    core = ['CS 231','CS 235','CS 240']
+def grabCourses(dept):
+    allCourses = []
     with open(majorReqs, "r") as courses:
         for row in courses:
-            if row[0].equals('Computer Science'):
-    electives = ['CS 203','CS 204','CS 220','CS 221','CS 232','CS 234','CS 242','CS 250','CS 250H','CS 251','CS 221','CS 301','CS 304']
+            rho = row.split('\t')
+            if (dept).__eq__(rho[0]):
+                allCourses = list(set(rho[1:]))
+                allCourses.sort()
+    return(allCourses)
+
+def findElectives(requiredList, allCoursesList):
+    electiveList = []
+    for element in allCoursesList:
+        if len(element) < 6:
+            pass
+        elif element == '\n':
+            pass
+        elif element not in requiredList:
+            electiveList.append(element)
+    return(electiveList)
+
+def courseLevelUntangler(electiveList, level):
+    levelElectives = []
+    for elective in electiveList:
+        courseNum = int(elective.split(" ")[1][0])
+        if courseNum == level:
+            levelElectives.append(elective)
+    return(levelElectives)
+
+def compareUserAndReqs(user, reqList, reqName, needed):
+    has = len(user)
+    count = 1
+    for course in user:
+        print(course, 'fulfills', count, 'of', needed, reqName, 'requirements')
+        count += 1
+    if needed <= has:
+        print("You've fulfilled all", reqName, "requirements for the major!\n")
+    else:
+        print('You need', (needed - has), 'more courses to fulfill all', reqName, "requirements for the major!\n")
+
+def cs(userInput):
+    print(userInput)
+    needed = 10
+    has = 0
+    introductory = ['CS 111','CS 230']
+    math = ['MATH 225']
+    core = ['CS 231','CS 235','CS 240']
+    required = introductory + math + core
+    allCourses = grabCourses('Computer Science')
+    electives = findElectives(required, allCourses)
+    threes = courseLevelUntangler(electives,3)
+    numThrees = 0
+    numElectives = 0
+    introsTaken = []
+    mathTaken = []
+    coresTaken = []
+    threesTaken = []
+    electivesTaken = []
+    for course in userInput:
+        if course in introductory:
+            introsTaken.append(course)
+            has += 1
+        elif course in math:
+            mathTaken.append(course)
+            has += 1
+        elif course in core:
+            coresTaken.append(course)
+            has += 1
+        elif (course in threes) and (numThrees < 2):
+            threesTaken.append(course)
+            has += 1
+            numThrees += 1
+        elif (course in electives) and (numElectives < 2):
+            electivesTaken.append(course)
+            has += 1
+            numElectives += 1
+
+    compareUserAndReqs(introsTaken, introductory, 'introductory',2)
+    compareUserAndReqs(mathTaken, math, 'math',1)
+    compareUserAndReqs(coresTaken, core, 'core',3)
+    compareUserAndReqs(threesTaken, threes, '300-level elective',2)
+    compareUserAndReqs(electivesTaken, electives, '200 or 300-level elective',2)
+
+kat = ['ARTH 267','ES 267','CS 111','CS 220','CS 230','CS 231','CS 235','CS 240','CS 242','CS 301','CS 304','CS 342','FREN 101','FREN 102','FREN 201','FREN 202','HIST 245','HIST 220','JPN 290','MATH 205','MATH 206','MATH 223','MATH 225','NEUR 100','PHIL 215','POL1 200','WRIT 166']
+cs(kat)
